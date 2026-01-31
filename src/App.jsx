b31ea48df21be127f2316265dfb299de2eebd830
r34l-rudr44 <edu.rudraa@gmail.com>
2026-02-01 02:54:48 +0530

    v0.5: Added Portfolio Builder & Strategy Section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import HomePage from "./pages/Home";
 import DashboardPage from "./pages/Dashboard";
 import FrameworkPage from "./pages/Framework";
 import MechanismsPage from "./pages/Mechanisms";
+import PortfolioPage from "./pages/Portfolio";
 import "./App.css";
 
 export default function App() {
@@ -14,6 +15,7 @@ export default function App() {
         <Route path="/dashboard" element={<DashboardPage />} />
         <Route path="/framework" element={<FrameworkPage />} />
         <Route path="/mechanisms" element={<MechanismsPage />} />
+        <Route path="/portfolio" element={<PortfolioPage />} />
       </Routes>
     </BrowserRouter>
   );
